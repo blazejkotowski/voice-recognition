2f13bfa9aeb566c769d537e705352ca2cb5a39f0
Błażej Kotowski <blaziko@gmail.com>
2013-12-12 10:06:48 +0100

    initial

---
+++ recognize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import scipy as sp
+import numpy as np
+import os
+
+files = os.listdir('train/')
+
+if __name__ == '__main__':
+  for filename in files:
+    print filename
+  
