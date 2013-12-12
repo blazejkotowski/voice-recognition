060ac9142cd07c36551c276933f00ef3aa6b7634
Błażej Kotowski <blaziko@gmail.com>
2013-12-12 10:49:36 +0100

    plotting fft

--- recognize.py
+++ recognize.py
@@ -8,13 +8,31 @@ from pylab import *
 files = map(lambda x: 'train/' + x, os.listdir('train/'))
 
 if __name__ == '__main__':
-  for filename in files:
-    print filename
-    _,signal = wav.read(filename)
-    sp.fft(signal)
+  for filename1, filename2 in zip(files[2:], files[3:]):
+    signal1 = np.fromfile(open(filename1),np.int16)
+    signal1 = signal1[12:len(signal1)]
+
+    signal2 = np.fromfile(open(filename2),np.int16)
+    signal2 = signal2[12:len(signal2)]
+
+    freqs1 = abs(sp.fft(signal1))
+    freqs2 = abs(sp.fft(signal2))
+    # args = linspace(0, maxfreqs, , endpoint=False)
+
     subplot(211)
-    args = linspace(0, len(signal), len(signal), endpoint=False)
-    stem(args, signal, '-*')
+    plot(freqs1[1:len(freqs1)/2])
+    xlim(0, 23000)
+    ylabel(filename1)
+    # yscale('log')
+    # xscale('log')
+    # stem(args, freqs, '-*')
+    subplot(212)
+    plot(freqs2[1:len(freqs2)/2])
+    ylabel(filename2)
+    xlim(0, 23000)
+    # yscale('log')
+    show()
+
 
     break
 
