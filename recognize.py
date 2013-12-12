8b4b9448dfe2c85afa2c29f76b3b16cc812bc105
Błażej Kotowski <blaziko@gmail.com>
2013-12-12 10:28:03 +0100

    sdaas

--- recognize.py
+++ recognize.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 import scipy as sp
+from scipy.io import wavfile as wav
 import numpy as np
 import os
+from pylab import *
 
-files = os.listdir('train/')
+files = map(lambda x: 'train/' + x, os.listdir('train/'))
 
 if __name__ == '__main__':
   for filename in files:
     print filename
+    _,signal = wav.read(filename)
+    sp.fft(signal)
+    subplot(211)
+    args = linspace(0, len(signal), len(signal), endpoint=False)
+    stem(args, signal, '-*')
+
+    break
+
+    
   
